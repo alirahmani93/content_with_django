47d736620261b5ae803fccc27f4fd403bff3cb85
alirahmani <ali.rahmani@miare.ir>
2024-03-03 21:09:41 +0330

    feat(content): remove filters for performance issue

--- content/admin.py
+++ content/admin.py
@@ -6,12 +6,12 @@ from content.models.content import Content
 
 class ContentAdmin(admin.ModelAdmin):
     list_display = ['user', 'title', 'is_active', 'created_at', 'updated_at']
-    list_filter = ['user', 'title', 'is_active', 'created_at', 'updated_at']
+    list_filter = ['is_active', 'created_at', 'updated_at']
 
 
 class ContentScoreAdmin(admin.ModelAdmin):
     list_display = ['user', 'content', 'score', 'is_active', 'created_at', 'updated_at']
-    list_filter = ['user', 'content', 'score', 'is_active', 'created_at', 'updated_at']
+    list_filter = ['score', 'is_active', 'created_at', 'updated_at']
 
 
 admin.site.register(Content, ContentAdmin)
